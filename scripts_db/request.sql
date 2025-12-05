f43147b73693e0dc606ed7eab7ac5204cd3cf498
Zabka iz Aldi <97976952+ViacheTix@users.noreply.github.com>
2025-12-05 23:16:40 +0300

    Modify context

--- scripts_db/request.sql
+++ scripts_db/request.sql
@@ -1,3 +1,21 @@
-SELECT disease_group, avg_cost_per_prescription, avg_cost_per_patient 
-FROM insight_cost_by_disease 
-ORDER BY avg_cost_per_prescription DESC;
+SELECT 
+    p.район_проживания, 
+    COUNT(*) as cnt
+FROM 
+    prescriptions pr
+JOIN 
+    diagnoses d ON pr.код_диагноза = d.код_мкб
+JOIN 
+    patients p ON pr.id_пациента = p.id_пациента
+WHERE 
+    d.название_диагноза ILIKE '%нос%' 
+    OR d.название_диагноза ILIKE '%ринит%' 
+    OR d.название_диагноза ILIKE '%синусит%' 
+    OR d.название_диагноза ILIKE '%рот%' 
+    OR d.название_диагноза ILIKE '%стоматит%' 
+    OR d.название_диагноза ILIKE '%гингивит%'
+GROUP BY 
+    p.район_проживания
+ORDER BY 
+    cnt DESC
+LIMIT 1;
